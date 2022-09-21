8f0d27beb20b8dde6b25661470364d42fd5ad625
Egg <adrianjimenez1950@gmail.com>
2022-09-20 21:58:19 -0400

    added code to serve client html files

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
-});
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -20,7 +17,7 @@ function filterByQuery(query, animalsArray) {
     if (query.personalityTraits) {
 
         if(typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits];
+            personalityTraitsArray = query.personalityTraits.split(',');
         } else {
             personalityTraitsArray = query.personalityTraits;
         }
@@ -76,7 +73,7 @@ function validateAnimal(animal) {
     }
 
     return true;
-}
+};
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -109,5 +106,26 @@ app.post('/api/animals', (req, res) => {
 
     }
 
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+    console.log(`URL: http://localhost:${PORT}`);
 });
 
